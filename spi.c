d4812cd1100941d43e5a71d932e7ae0d76efde47
Paul Annesley <paul@annesley.cc>
2017-08-13 23:44:21 +1000

    spi: fastest; 8MHz, was 1MHz.

--- spi.c
+++ spi.c
@@ -18,12 +18,12 @@ void spi_init() {
       (0<<DORD)| // Data Order (0:MSB first / 1:LSB first)
       (1<<MSTR)| // Master/Slave select
       (0<<SPR1)| // SPI Clock Rate
-      (1<<SPR0)| // SPI Clock Rate
+      (0<<SPR0)| // SPI Clock Rate
       (0<<CPOL)| // Clock Polarity (0:SCK low / 1:SCK hi when idle)
       (0<<CPHA)| // Clock Phase (0:leading / 1:trailing edge sampling)
       0);
 
-  // SPSR = (1<<SPI2X);  // Double Clock Rate
+  SPSR = (1<<SPI2X);  // Double Clock Rate
 }
 
 void spi_send_8(uint8_t data) {
